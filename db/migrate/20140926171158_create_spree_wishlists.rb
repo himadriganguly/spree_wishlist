bc9f633f72efcf4a5a90af656394492739330e62
Himadri Ganguly <himadri@colourdrift.com>
2014-09-28 15:44:52 +0530

    Changed migration file

--- db/migrate/20140926171158_create_spree_wishlists.rb
+++ db/migrate/20140926171158_create_spree_wishlists.rb
@@ -1,6 +1,8 @@
 class CreateSpreeWishlists < ActiveRecord::Migration
   def change
     create_table :spree_wishlists do |t|
+      t.references :user
+      t.references :product
 
       t.timestamps
     end
